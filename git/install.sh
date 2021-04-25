9fd636b257f973d572c7388efad067805d3c15b2
Eusebio Ajas Santos <eusebioajas6@gmail.com>
2021-04-25 00:56:34 -0500

    update install.sh

--- git/install.sh
+++ git/install.sh
@@ -1,8 +1,20 @@
-# Create a new link called ~.gitconfig which comes from ~/(pwd)/.gitconfig
+#!/bin/sh
+
+# Check if git is installed
+if which git >/dev/null; then
+  echo "git is installed...!"
+else
+  echo "Installing git..."
+  sudo add-apt-repository ppa:git-core/ppa
+  sudo apt update
+  sudo apt install git
+fi
+
+# Create a new link called .gitconfig which comes from ~/(pwd)/.gitconfig
 echo "Create symlink for gitconfig..." 
 ln -sf $(pwd)/.gitconfig ~/.gitconfig
 
-# Create a new link called ~.gitmessage which comes from ~/(pwd)/.gitmessage
+# Create a new link called .gitmessage which comes from ~/(pwd)/.gitmessage
 echo "Create symlink for gitmessage..."
 ln -sf $(pwd)/.gitmessage ~/.gitmessage
 
