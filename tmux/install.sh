aaf03856974a0d8d822ed53653e1b3f85aa998b1
Eusebio Ajas Santos <eusebioajas6@gmail.com>
2021-05-08 16:05:21 -0500

    update tmux setup

--- tmux/install.sh
+++ tmux/install.sh
@@ -2,7 +2,7 @@
 
 #Check if tmux is installed
 if which tmux >/dev/null; then
-    echo "tmux is installed...!"
+    echo "tmux is already installed...!"
 else
     echo "Installing tmux..."
     sudo apt install tmux
