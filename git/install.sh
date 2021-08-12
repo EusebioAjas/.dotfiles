206cb982fc0787f106a9129c05dff1d120c81061
Eusebio Ajas Santos <eusebioajas6@gmail.com>
2021-08-12 14:32:48 -0500

    Update git installer
    - raplace hash by command

--- git/install.sh
+++ git/install.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 
 # Check if git is installed
-if which git >/dev/null; then
-  echo "git is installed...!"
-else
+if [ command git 2>/dev/null ]; then
   echo "Installing git..."
   sudo add-apt-repository ppa:git-core/ppa
   sudo apt update
   sudo apt install git
+  # Create a new link called .gitconfig which comes from ~/(pwd)/.gitconfig
+  echo "Create symlink for gitconfig..." 
+  ln -sf $(pwd)/.gitconfig ~/.gitconfig
+
+  # Create a new link called .gitmessage which comes from ~/(pwd)/.gitmessage
+  echo "Create symlink for gitmessage..."
+  ln -sf $(pwd)/.gitmessage ~/.gitmessage
+else
+  echo "git is installed...!"
+  exit 1;
 fi
 
-# Create a new link called .gitconfig which comes from ~/(pwd)/.gitconfig
-echo "Create symlink for gitconfig..." 
-ln -sf $(pwd)/.gitconfig ~/.gitconfig
-
-# Create a new link called .gitmessage which comes from ~/(pwd)/.gitmessage
-echo "Create symlink for gitmessage..."
-ln -sf $(pwd)/.gitmessage ~/.gitmessage
 
