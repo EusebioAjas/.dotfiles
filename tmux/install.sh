78ab7230b167c8c465e6f9e9d40efee3e7679a15
Eusebio Ajas Santos <eusebioajas6@gmail.com>
2021-09-05 19:54:26 -0500

    Update script for tmux installation

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 # Check if tmux is installed
-if [ command tmux 2>/dev/null ]; then
-    echo "Not installed"
+if ! command -v tmux &> /dev/null; then
+    echo "Installing tmux..."
     sudo apt install tmux
     # Create a new link called .tmux.conf which comes from ~/(pwd)/.tmux.conf
     echo "Create symlink for tmux configuration..."
-    ln -sf ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
+    ln -sf ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
     # dir for tmux-plugins
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 else
-    echo "tmux is installed...!"
+    echo "tmux is already installed!"
     exit 1;
 fi
 
